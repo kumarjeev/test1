f6a6c21acc7f3ea431e4e612e35ad9415988bfd2
kumarjeev <THATZJEEVANKUMAR@GMAIL.COM>
2026-01-16 17:44:16 +0530

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		char ch='a';
+		boolean isTr=true;
+		float f=10.8f;
 		SpringApplication.run(DemoApplication.class, args);
                 int y=90;
 		int x=99;
