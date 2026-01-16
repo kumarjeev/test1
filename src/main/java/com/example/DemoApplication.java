ae9def63e906849983b381d44423c49f0cc62e8e
kumarjeev <thatzjeevankumar@gmail.com>
2026-01-16 17:46:55 +0530

    bug-fix-345

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,6 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		char ch='a';
 		boolean isTr=true;
+		System.out.priint("mike");
+		System.out.priint("kam");
+		System.out.priint("adam");
 		float f=10.8f;
 		SpringApplication.run(DemoApplication.class, args);
                 int y=90;
